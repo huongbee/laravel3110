56c05a19cdf63d0c7a32503e6f9f584f9d88b42e
Huong <you@example.com>
2018-03-08 19:46:14 +0700

    register form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,17 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
+
 <body>
-    
-    <h2>Register Form</h2>
+    {{-- name email password password_confirm address phone --}} {{--$book--}}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-6">
+                <h2>Register Form</h2>
+                <form method="POST">
+                    <div class="form-group">
+                        <label for="name">Fullname</label>
+                        <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Enter fullname" name="fullname">
 
-    {{$book}}
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Confirm Password</label>
+                        <input type="password" class="form-control" placeholder="Confirm Password" name="confirm_password">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Address</label>
+                        <input type="text" class="form-control" placeholder="Address" name="address">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Phone</label>
+                        <input type="text" class="form-control" placeholder="Phone" name="phone">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
 
-    
+    </div>
 </body>
+
 </html>
